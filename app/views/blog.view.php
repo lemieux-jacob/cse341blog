f985e5240d220f90ce7fa48fcc76181f1e5cecdc
FishiSushi <bocaj.xml@gmail.com>
2020-05-23 10:56:37 -0400

    ux improvements

--- app/views/blog.view.php
+++ app/views/blog.view.php
@@ -8,19 +8,19 @@
 
 <?php foreach($posts as $post): ?>
 <div class="card rounded-0 my-3">
-  <a href=/posts/view?id=<?= $post->id ?>">
-    <div class="card-body bg-gradient-dark">
+  <div class="card-body bg-gradient-dark">
+    <a href=/posts/view?id=<?= $post->id ?>">
       <h3 class="display-4"><?= $post->title; ?></h3>
+    </a>
+    <div class="d-flex">
+      <div class="small text-muted p-3">
+        Posted on: <?= $post->posted_on; ?> By: <?= $post->author()->display_name; ?>
+      </div>
+      <a class="btn btn-primary ml-auto" href="/posts/view?id=<?= $post->id ?>">View Post</a>
+      <?php if (user() && user()->isAdmin()): ?>
+      <a class="btn btn-primary mx-1" href="/posts/edit?id=<?= $post->id ?>">Edit Post</a>
+      <?php endif; ?>
     </div>
-  </a>
-  <div class="d-flex">
-    <div class="small text-muted p-3">
-      Posted on: <?= $post->posted_on; ?> By: <?= $post->author()->display_name; ?>
-    </div>
-    <a class="btn btn-primary ml-auto" href="/posts/view?id=<?= $post->id ?>">View Post</a>
-    <?php if (user() && user()->isAdmin()): ?>
-    <a class="btn btn-primary mx-1" href="/posts/edit?id=<?= $post->id ?>">Edit Post</a>
-    <?php endif; ?>
   </div>
 </div>
 <?php endforeach; ?>
